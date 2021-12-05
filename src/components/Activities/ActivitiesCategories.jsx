1bb7bd842deb8862c4c5463b7e96ebea3a0735ef
dukizwe <mbagapro@gmail.com>
2021-12-05 13:28:25 +0200

    feat(activitiesCategories): customize selected categories

--- src/components/Activities/ActivitiesCategories.jsx
+++ src/components/Activities/ActivitiesCategories.jsx
@@ -1,36 +1,54 @@
-import React from 'react'
-import { View, Text, StyleSheet  } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableWithoutFeedback  } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import { FontAwesome5 } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons'; 
 
 export default function ActivitiesCategories() {
+          const categories = [
+                    {
+                              label: 'Agape',
+                              color: '#873475'
+                    },
+                    {
+                              label: 'Aide',
+                              color: '#362b89ed'
+                    },
+                    {
+                              label: 'Sacrifice',
+                              color: '#ffc107f5'
+                    },
+                    {
+                              label: 'Autres',
+                              color: '#40c2d7f5'
+                    }
+          ]
+          const [selected, setSelected] = useState([])
+          const toggleSelect = (index) => {
+                    if(selected.includes(index)) {
+                              const newSelected = selected.filter(i => i != index)
+                              setSelected(newSelected)
+                    } else {
+                              setSelected(prev => [...prev, index])
+                    }
+          }
           return (          
                     <View style={styles.quickActions}>
-                              <View style={styles.action}>
-                                        <View style={styles.actionIcon}>
-                                                  <FontAwesome5 name="tasks" size={20} color='#fff' />
-                                        </View>
-                                        <Text style={styles.actionTitle}>Agape</Text>
-                              </View>
-                              <View style={styles.action}>
-                                        <View style={{...styles.actionIcon, backgroundColor: '#362b89ed'}}>
-                                                  <Feather name="bell" size={24} color="#fff" />
-                                        </View>
-                                        <Text style={styles.actionTitle}>Aide</Text>
-                              </View>
-                              <View style={styles.action}>
-                                        <View style={{...styles.actionIcon, backgroundColor: '#ffc107f5'}}>
-                                                  <AntDesign name="barschart" size={24} color="#fff" />
-                                        </View>
-                                        <Text style={styles.actionTitle}>Sacrifice</Text>
-                              </View>
-                              <View style={styles.action}>
-                                        <View style={{...styles.actionIcon, backgroundColor: '#40c2d7f5'}}>
-                                                  <FontAwesome5 name="tasks" size={20} color='#fff' />
-                                        </View>
-                                        <Text style={styles.actionTitle}>Autres</Text>
-                              </View>
+                              {categories.map((category, i) => {
+                                        return (
+                                                  <TouchableWithoutFeedback onPress={() => toggleSelect(i)}>
+                                                            <View>
+                                                            <View style={{...styles.action, opacity: selected.includes(i) ? 0.6 : 1}} key={i.toString()}>
+                                                                      <View style={{...styles.actionIcon, backgroundColor: category.color}}>
+                                                                                <Feather name="bell" size={24} color="#fff" />
+                                                                      </View>
+                                                                      <Text style={styles.actionTitle}>{category.label}</Text>
+                                                            </View>
+                                                            {selected.includes(i) && <View style={{...styles.selectedCheck, backgroundColor: category.color}}>
+                                                                      <Feather name="check" size={19} color="#fff" style={{marginTop: -1, marginLeft: -2}} />
+                                                            </View>}
+                                                            </View>
+                                                  </TouchableWithoutFeedback>
+                                        )}
+                              )}
                     </View>
           )
 }
@@ -49,7 +67,6 @@ const styles = StyleSheet.create({
           },
           actionIcon: {
                     borderRadius: 20,
-                    backgroundColor: '#873475',
                     padding: 20,
                     justifyContent: 'center',
                     alignItems: 'center',
@@ -61,4 +78,17 @@ const styles = StyleSheet.create({
                     opacity: 0.6,
                     fontWeight: 'bold'
           },
+          selectedCheck: {
+                    width: 20,
+                    height: 20,
+                    backgroundColor: 'red',
+                    borderRadius: 100,
+                    position: 'absolute',
+                    top: -10,
+                    right: 0,
+                    padding: 2,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignContent: 'center'
+          }
 })

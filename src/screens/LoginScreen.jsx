c3fb38798b3cfab00db9f70048ca1388e8e24f9c
dukizwe <mbagapro@gmail.com>
2022-08-22 08:44:10 +0200

    refactory(styles): test the keyboardShouldPersistTaps: 'always' on react-native-modalize test if we can handle taps in modalize modal

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Login from "../components/Login/Login";
+import { Modalize } from 'react-native-modalize';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Input } from 'native-base';
 
 export default function LoginScreen() {
-          return <Login />
+          const modalizeRef = useRef(null);
+          const onOpen = () => {
+                    modalizeRef.current?.open();
+          };
+          return (
+                    <>
+                    {/* <Login /> */}
+                    <TouchableOpacity onPress={onOpen}>
+                              <Text>Open the modal</Text>
+                    </TouchableOpacity>
+
+                              <Modalize
+                                        ref={modalizeRef}
+                                        adjustToContentHeight
+                                        scrollViewProps={{
+                                                  keyboardShouldPersistTaps: 'always'
+                                        }}
+                              >
+                                        <View >
+                                                  <TouchableOpacity onPress={() => modalizeRef.current?.close()}>
+                                                            <Text>Close</Text>
+                                                  </TouchableOpacity>
+                                                  <Text style={{ marginVertical: 100 }}>hey there</Text>
+                                                  <Input />
+                                        </View>
+                              </Modalize>
+                    </>
+          )
 }
